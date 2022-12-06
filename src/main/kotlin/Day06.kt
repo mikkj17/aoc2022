2e2f36f9f818c2f89376502f7e7afef7955a5017
Mikkel <mikkel@housekeepr.dk>
2022-12-06 08:11:27 +0100

    solve day 6

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,30 @@
+import java.io.File
+
+private val test = """
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb
+""".trimIndent()
+
+private fun compute(inp: String, length: Int): Int {
+    var index = length
+    for (window in inp.windowed(length)) {
+        if (window.toSet().size == length) {
+            break
+        }
+        index++
+    }
+    return index
+}
+
+private fun partOne(inp: String): Int {
+    return compute(inp, 4)
+}
+
+private fun partTwo(inp: String): Int {
+    return compute(inp, 14)
+}
+
+fun main() {
+    val inp = File("src/main/resources/day06.txt").readText().trim()
+    println(partOne(inp))
+    println(partTwo(inp))
+}
